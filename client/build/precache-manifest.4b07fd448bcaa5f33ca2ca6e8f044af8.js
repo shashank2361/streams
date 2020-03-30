d032c7631312af0bf4e7022e696243ce667c8abc
DESKTOP-SB3UMDG\Shashank-PC <shashank2361@yahoo.co.in>
2020-03-30 17:26:38 +0100

    npm

---
+++ client/build/precache-manifest.4b07fd448bcaa5f33ca2ca6e8f044af8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc510fa1cb9aabf6b552206aaa7d45db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7f513b84b6166015cf4",
+    "url": "/static/js/2.f5e15e5e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.f5e15e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399e9a8ba4dc69813515",
+    "url": "/static/js/main.aa31d362.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
